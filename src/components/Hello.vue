eb6612a043dbe64b131d878546f3219aabe3a7df
Lucien Lee <lkiral7903@gmail.com>
2016-10-05 23:01:29 +0800

    add jade and sass

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -15,8 +15,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1 {
-  color: #42b983;
-}
+<style lang="sass" scoped>
+h1
+	color: #42b983
 </style>
