6863ba3a0b7615458feb8a3d78547f37b9e487c1
DaxChen <tp6m4cj96@gmail.com>
2016-10-11 23:34:58 +0800

    add fonts `Noto Sans` and `Noto Sans TC`

--- src/main-library-desktop.js
+++ src/main-library-desktop.js
@@ -4,6 +4,10 @@ import Vue from 'vue'
 import App from './App'
 import store from './store'
 
+// import fonts here because this is the electron app
+import './assets/fonts/Noto_Sans_TC/NotoSansTC.scss'
+import './assets/fonts/Noto_Sans/NotoSans.scss'
+
 import 'normalize.css'
 
 /* eslint-disable-next-line no-new */
