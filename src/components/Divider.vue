d5d0458ce360a7feacff18753073d84f0b450c7f
Lucien Lee <lkiral7903@gmail.com>
2016-10-11 20:47:30 +0800

    clean files

---
+++ src/components/Divider.vue
@@ -0,0 +1,11 @@
+<template lang="jade">
+	.divider
+</template>
+
+<style lang="sass">
+.divider
+	width: 100%
+	height: 1px
+	border: 1px solid #E4E4E4
+	box-shadow: 0px -1px 0px 0px rgba(0,0,0,0.19), 0px -1px 0px 0px rgba(137,137,137,0.07)
+</style>>
