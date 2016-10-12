537c20f85b0db53f6cce9fd7b2376e24101ff188
Lucien Lee <lkiral7903@gmail.com>
2016-10-13 00:07:55 +0800

    Done Divider Component

--- src/components/Divider.vue
+++ src/components/Divider.vue
@@ -1,11 +1,29 @@
 <template lang="jade">
-	.divider
+	.divider(:class="[ type ? 'divier--' + type : '' ]")
 </template>
+
+<script>
+export default {
+	name: 'divider',
+	props: {
+		type: {
+			type: String,
+			default: () => {
+                return null
+            }
+		}
+	}
+}
+</script>
 
 <style lang="sass">
 .divider
+	box-sizing: border-box
 	width: 100%
 	height: 1px
-	border: 1px solid #E4E4E4
-	box-shadow: 0px -1px 0px 0px rgba(0,0,0,0.19), 0px -1px 0px 0px rgba(137,137,137,0.07)
+	border-top: 1px solid #E4E4E4
+	box-shadow: 0px 1px 0px 0px rgba(0,0,0,0.12)
+
+.divier--strong
+	box-shadow: 0px -1px 0px 0px rgba(0,0,0,0.19), 0px 1px 0px 0px rgba(137,137,137,0.07)
 </style>>
