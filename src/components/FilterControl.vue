6d1cafe01087147230fbb11efae3f5ca9ce9bff4
Lucien Lee <lkiral7903@gmail.com>
2016-10-21 16:20:42 +0800

    refactor scretch style

--- src/components/FilterControl.vue
+++ src/components/FilterControl.vue
@@ -26,7 +26,7 @@ export default {
 
 <style lang="sass">
 @import '../sass/variables'
-@import '../sass/mixin'
+@import "../sass/mixin"
 
 $leading: $panel-leading + 2px
 
@@ -53,10 +53,6 @@ $leading: $panel-leading + 2px
 	padding-bottom: percentage($button-height / $panel-inner-width)
 
 .filter-inputs
-	position: absolute
-	top: 0
-	bottom: 0
-	left: 0
-	right: 0
+	+stretch
 
 </style>>
