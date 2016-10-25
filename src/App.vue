baac4b7db815c3968947cb535418e3097c1437c7
Lucien Lee <lkiral7903@gmail.com>
2016-10-25 20:27:36 +0800

    remove test case

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,6 @@
 	.controlsContainer
 		seat-register
 		seat-filter
-	button(@click="hello") open!!!
-	br
-	button(@click="bb") error!!!
 </template>
 
 <script>
@@ -21,32 +18,6 @@ export default {
 		SeatFilter,
 		SeatRegister,
 		Modal
-	},
-	data (){
-		return {
-			open : false,
-			message: {
-				studentID: 'B98701034',
-				seatID: 'A001',
-				zh: '違規管制期間，您無法使用自習室!',
-				en: 'You are temporarily banned from using the Study Room due to violation of library regulations.'
-			}
-		}
-	},
-	methods: {
-		hello () {
-			this.$modal({
-				type: 'success',
-				message: this.message,
-				timer: 20
-			})
-		},
-		bb() {
-			this.$modal({
-				type: 'failure',
-				message: this.message
-			})
-		}
 	}
 }
 </script>
