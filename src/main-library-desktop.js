306b7a205fcf902c2f3d31957c19730d8c63d1d0
DaxChen <tp6m4cj96@gmail.com>
2016-10-25 14:52:28 +0800

    query for all seats, add a skeleton for kelly in MapContainer

--- src/main-library-desktop.js
+++ src/main-library-desktop.js
@@ -18,3 +18,42 @@ new Vue({
   render: h => h(App)
 })
 
+
+
+
+
+
+/**
+ * graphql client with vuex sync
+ */
+import gql from 'graphql-tag'
+import apolloClient from './apolloClient'
+import { updateSeats } from './store/actions'
+
+// function arr2obj(arr, key) {
+// 	return arr.reduce((o, v) => {
+// 		o[v[key]] = v
+// 		return o
+// 	}, {})
+// }
+
+apolloClient.watchQuery({
+	query: gql`
+		{
+			all_seats {
+				seat_id
+				status
+			}
+		}
+	`,
+	pollInterval: 300, // ms
+}).subscribe({
+	next ({ data }) {
+		// const obj = arr2obj(data.all_seats, 'seat_id')
+		updateSeats(store, data.all_seats)
+	},
+	// error (err) {
+	// 	console.error(err)
+	// }
+})
+
---
+++ src/store/modules/seats.js
@@ -0,0 +1,21 @@
+// import * as types from '../mutation-types'
+
+const state = {
+}
+
+// mutations
+const mutations = {
+	// UPDATE_SEATS (state, val) {
+	// 	state.laptop = val
+	// },
+}
+
+const actions = {
+}
+
+export default {
+	state,
+	mutations,
+	actions,
+}
+
