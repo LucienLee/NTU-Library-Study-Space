73712d2bcf539bf1f67ed1d4cdf3a4bd2217a8ae
Lucien Lee <lkiral7903@gmail.com>
2016-10-26 15:35:55 +0800

    Add Input Validate

--- src/components/SeatRegister.vue
+++ src/components/SeatRegister.vue
@@ -2,13 +2,14 @@
 .seat-register
 	panel(:headerTitle="title")
 		div(slot="panel-body")
-			text-field(v-for="field in fields", :id="field.id", :placeholder="field.placeholder", v-model="field.value")
+			text-field(v-for="field in fields", :id="field.id", :placeholder="field.placeholder", :pattern="field.pattern", v-model="field.value")
 </template>
 
 <script>
 import Panel from './Panel'
 import TextField from './TextField'
 import Divider from './Divider'
+
 
 
 export default {
@@ -20,12 +21,13 @@ export default {
 	data: () => ({
 		title: {
 			zh: '登記你的自習座位',
-			en: 'Register your Seat'
+			en: 'Register your Seat',
 		},
 		fields: [
 			{
 				id: 'studentID',
 				value: '',
+				pattern: /[a-zA-Z]+\d{8}$/,
 				placeholder: {
 					zh: '刷卡輸入學生證號',
 					en: 'Scan student card to enter ID'
@@ -33,13 +35,17 @@ export default {
 			}, {
 				id: 'seatNumber',
 				value: '',
+				pattern: /[A-Z]+\d{3}$/,
 				placeholder: {
 					zh: '點選地圖來選擇座位',
 					en: 'Select seat from the map'
 				}
 			}
 		]
-	})
+	}),
+	methods: {
+
+	}
 }
 </script>
 
