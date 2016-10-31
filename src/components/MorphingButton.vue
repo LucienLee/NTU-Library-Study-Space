8f10f1ff4d22c3f86bc1b2dea1380f0b273f8c42
Lucien Lee <lkiral7903@gmail.com>
2016-10-31 22:06:04 +0800

    Add morphing button

---
+++ src/components/MorphingButton.vue
@@ -0,0 +1,88 @@
+<template lang="jade">
+transition(@enter="expand" @leave="collapse")
+	button.MorphingButton(v-show="show", @click.stop="onClick")
+		icon.MorphingButton__label(symbol="check", ref="icon")
+</template>
+
+<script>
+import { TimelineMax, Back } from 'gsap'
+
+import Icon from './Icon.vue'
+
+const iconSize = 32
+
+export default {
+	props: {
+		show: Boolean
+	},
+	components: {
+		Icon
+	},
+	mounted () {
+		let button = this.$el,
+			icon = this.$refs.icon
+
+		this.timeline = new TimelineMax({ paused: true })
+
+		this.timeline.from(button, 0.4, {
+			width: iconSize,
+			height: iconSize,
+			borderRadius: iconSize/2,
+			borderWidth: 4,
+			ease: Back.easeOut.config(2)
+		}).from(icon, 0.4, {
+			autoAlpha: 0
+		}, 0).from(icon, 0.2, {
+			scale: 0.8,
+			ease: Back.easeOut.config(1.6)
+		}, '-=0.2')
+	},
+	methods: {
+		expand (el, done) { // eslint-disable-line no-unused-vars
+			this.timeline.vars.onComplete = done
+			this.timeline.play()
+		},
+		collapse (el, done) { // eslint-disable-line no-unused-vars
+			this.timeline.vars.onReverseComplete = done
+			this.timeline.reverse()
+		},
+		onClick () {
+			this.$emit('click')
+		}
+	}
+}
+</script>
+
+<style lang="sass">
+@import '../sass/variables'
+@import '../sass/mixin'
+
+$primary-color: #FFA55C
+$background-color: #FFF5EB
+
+.MorphingButton
+	+clearInputStyle
+	position: absolute
+	box-sizing: border-box
+	border-width: $button-border
+	border-style: solid
+	border-color: $primary-color
+	background: #FFF5EB
+	width: 100%
+	height: 100%
+	border-radius: $border-radius
+	text-align: center
+	cursor: pointer
+	transition: background $fast $easeIn
+	will-change: width, height, border-width // accelerate expensive animation
+
+	&:hover
+		background: darken( $background-color, 4% )
+
+
+.MorphingButton__label
+	box-sizing: border-box
+	padding: 6px
+	color: $primary-color
+
+</style>
