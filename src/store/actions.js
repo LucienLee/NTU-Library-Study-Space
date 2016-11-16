ad57cd0f6dba67d098066be24dc50d0df5b23da4
DaxChen <tp6m4cj96@gmail.com>
2016-11-16 22:22:44 +0800

    move module/seats -> store/index.js

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,5 +1,24 @@
+import gql from 'graphql-tag'
+import apolloClient from '../apolloClient'
 
 export function updateSeats ({ commit }, val) {
 	commit('UPDATE_SEATS', val)
 }
 
+export function startSeatQuery ({ dispatch }) {
+	apolloClient.watchQuery({
+		query: gql`
+			{
+				seatInfo
+			}
+		`,
+		pollInterval: 300, // ms
+	}).subscribe({
+		next ({ data }) {
+			dispatch('updateSeats', JSON.parse(data.seatInfo))
+		},
+		error (err) {
+			console.error(err) // eslint-disable-line no-console
+		}
+	})
+}
